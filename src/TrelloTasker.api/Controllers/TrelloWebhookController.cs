3943617c6168997a7f1c11259b1bc11132047e54
Mer Moyano <moyanomariamercedes@gmail.com>
2020-08-22 00:34:49 -0300

    Added webhook for trello

---
+++ src/TrelloTasker.api/Controllers/TrelloWebhookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrelloTasker.Model.DTO;
+
+namespace TrelloTasker.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrelloWebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> OnWebhookCallback(TrelloWebHookDTO trelloWebhookDTO)
+        {
+            if (trelloWebhookDTO.action.data.card.name.EndsWith("Bi-daily"))
+            {
+                throw new NotImplementedException();
+            }
+            return new OkObjectResult(trelloWebhookDTO);
+        }
+    }
+}
