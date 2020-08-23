6975f9c69b1853f5fa7bcf046a0f27aa876718b8
Mer Moyano <moyanomariamercedes@gmail.com>
2020-08-23 02:20:04 -0300

    Swagger and logger

--- src/TrelloTasker.api/Startup.cs
+++ src/TrelloTasker.api/Startup.cs
@@ -26,6 +26,7 @@ namespace TrelloTasker.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,12 @@ namespace TrelloTasker.api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swager/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
