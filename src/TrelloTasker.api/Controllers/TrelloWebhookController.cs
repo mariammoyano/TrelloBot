6dc72772a7c5a7b4aadaa5c5635b5ddeb5c14305
Mer Moyano <moyanomariamercedes@gmail.com>
2020-08-23 02:20:04 -0300

    Swagger and logger

--- src/TrelloTasker.api/Controllers/TrelloWebhookController.cs
+++ src/TrelloTasker.api/Controllers/TrelloWebhookController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TrelloTasker.Model.DTO;
 
 namespace TrelloTasker.api.Controllers
@@ -12,9 +12,19 @@ namespace TrelloTasker.api.Controllers
     [ApiController]
     public class TrelloWebhookController : ControllerBase
     {
+    
+        private readonly ILogger<TrelloWebhookController> logger;
+
+        public TrelloWebhookController(ILogger<TrelloWebhookController> logger)
+        {
+            this.logger = logger;
+        }
+    
         [HttpPost]
         public async Task<IActionResult> OnWebhookCallback(TrelloWebHookDTO trelloWebhookDTO)
         {
+            logger.LogInformation(Request.Body.ToString(), Request);
+
             if (trelloWebhookDTO.action.data.card.name.EndsWith("Bi-daily"))
             {
                 throw new NotImplementedException();
